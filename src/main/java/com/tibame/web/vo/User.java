00fd21403f0193cc2c97edb6450ebd2196f3dbb9
r05323039 <r05323039@gmail.com>
2023-02-06 09:34:33 +0800

    forum folder, login

---
+++ src/main/java/com/tibame/web/vo/User.java
@@ -0,0 +1,51 @@
+package com.tibame.web.vo;
+
+public class User {
+    private int userId;
+    private String userName;
+    private String password;
+
+    public User(int userId, String userName, String password) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
