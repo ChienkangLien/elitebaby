26df7aa5777775b380881005f0c5826e87df347a
Sam <chiang1369@gmail.com>
2023-03-01 15:36:38 +0800

    fix: 新增前端頁面的路徑
    調整項目:
    把千港的房型相關的路徑加到herf標籤裡面

--- src/main/webapp/admin/visit/js/back_visit_callender.js
+++ src/main/webapp/admin/visit/js/back_visit_callender.js
@@ -2,13 +2,33 @@
 
 $(document).ready(function() {
     $('#calendar').fullCalendar({
-      locales: 'zh-TW', 
-      lang: 'zh-tw',
+      locale: 'zh-tw', 
       header: {
         left: 'prev,next today',
         center: 'title',
         right: 'month,agendaWeek,agendaDay'
       },
+      buttonText: { //各按钮的显示文本信息
+        today: '今天',
+        month: '月',
+        agendaWeek: '周',
+        agendaDay: '日',
+        listMonth: '日程',
+    },
+    //      columnFormat: {
+    //       month: 'dddd',
+    //       week: 'dddd M-d',
+    //       day: 'dddd M-d'
+    //   },
+    //titleFormat: {
+    //       month: 'yyyy年 MMMM月',
+    //       week: "[yyyy年] MMMM月d日 { '―' [yyyy年] MMMM月d日}",
+    //       day: 'yyyy年 MMMM月d日 dddd',
+    //   },
+    monthNames: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"], //月份全称
+    monthNamesShort: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"], //月份简写
+    dayNames: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"], //周全称
+    dayNamesShort: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"], //周简写
       events: [], // 空的 events 陣列，等待 fetch 回傳事件資料後加入
       eventClick: function(event) {
         alert('Event: ' + event.title);
