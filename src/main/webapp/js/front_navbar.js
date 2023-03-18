01542bc571809f90f95ce34da6ecc915691937ac
Leo <karta336429@gmail.com>
2023-03-18 11:19:00 +0800

    feat: 先將會員功能移除，打算從release開分支出來另外做修改；front_navbar.js有衝突做修改

--- src/main/webapp/js/front_navbar.js
+++ src/main/webapp/js/front_navbar.js
@@ -51,159 +51,220 @@ $(function () {
     checkLogin();
 });
 
-// ======================鈴鐺===========================
-var userId = 4;
+//===============================================鈴鐺==============================================
 
-var MyPoint = `/emailBell/${userId}`;
-var host = window.location.host;
-var path = window.location.pathname;
-var webCtx = path.substring(0, path.indexOf("/", 1));
-var endPointURL = "ws://" + window.location.host + webCtx + MyPoint;
 
-var webSocket;
 
-function get_email_websucket() {
-    webSocket = new WebSocket(endPointURL);
+fetch('/elitebaby/visit/servlet?action=GET_MEMBER_INFO', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+})
+    .then(resp => resp.json())
+    .then(data => {
 
-    webSocket.onopen = function (event) {
-        console.log("Connect Success!");
-    };
+        var userId = data.id;
 
-    webSocket.onmessage = function (event) {
-        var jsonObj = JSON.parse(event.data);
-        console.log(jsonObj);
-        console.log(jsonObj.from);
 
-        if (typeof jsonObj.from === "string") {
-            var bellcounter = parseInt(
-                document.querySelector(".emailBill").outerText
-            );
-            console.log(bellcounter);
-            if (bellcounter > 0 && bellcounter !== "undefined") {
-                bellcounter += 1;
-                document.querySelector(".emailBill").innerText = `${bellcounter}`;
-            } else {
-                document.querySelector(".emailBill").innerText = "1";
-            }
-        } else {
-            // 隱藏提示沒有新郵件
+
+        console.log(userId);
+
+        var MyPoint = `/emailBell/${userId}`;
+        var host = window.location.host;
+        var path = window.location.pathname;
+        var webCtx = path.substring(0, path.indexOf("/", 1));
+        var endPointURL = "ws://" + window.location.host + webCtx + MyPoint;
+
+        var webSocket;
+
+        function get_email_websucket() {
+            webSocket = new WebSocket(endPointURL);
+
+            webSocket.onopen = function (event) {
+                console.log("Connect Success!");
+            };
+
+            webSocket.onmessage = function (event) {
+                var jsonObj = JSON.parse(event.data);
+                console.log(jsonObj);
+                console.log(jsonObj.from);
+
+                if (typeof jsonObj.from === "string") {
+                    var bellcounter = parseInt(
+                        document.querySelector(".emailBill").outerText
+                    );
+                    console.log(bellcounter);
+                    if (bellcounter > 0 && bellcounter !== "undefined") {
+                        bellcounter += 1;
+                        document.querySelector(".emailBill").innerText = `${bellcounter}`;
+                    } else {
+                        document.querySelector(".emailBill").innerText = "1";
+                    }
+                } else {
+                    // 隱藏提示沒有新郵件
+                }
+            };
+
+            webSocket.onclose = function (event) {
+                console.log("Disconnected!");
+            };
         }
-    };
 
-    webSocket.onclose = function (event) {
-        console.log("Disconnected!");
-    };
-}
+        if (userId != null) {
+            get_email_websucket();
+        }
 
-if (userId != null) {
-    get_email_websucket();
-}
-
-fetch(
-    `/elitebaby/report/emailservlet?action=get_email_redis&userId=user${userId}`,
-    {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }
-)
-    .then((resp) => resp.json())
-    .then((data) => {
-        let emailBellData = [];
-        emailBellData = data;
-        console.log(data);
-        var bell_count;
-        if (data.length == 1) {
-            if (emailBellData[0].status === "unread") {
-                bell_count = parseInt(emailBellData[0].unreadCount);
-                console.log(emailBellData[0].unreadCount);
-                document.querySelector(".emailBill").innerText = `${bell_count}`;
-            } else {
-                document.querySelector(".emailBill").innerText = "";
+        fetch(
+            `/elitebaby/report/emailservlet?action=get_email_redis&userId=user${userId}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
             }
-        } else if (data.length == 2) {
-            if (emailBellData[0].status === "unread") {
-                var bell_count1 = parseInt(emailBellData[0].unreadCount);
-                console.log(bell_count1);
-            } else {
-                var bell_count1 = 0;
-                document.querySelector(".emailBill").innerText = "";
-            }
+        )
+            .then((resp) => resp.json())
+            .then((data) => {
+                let emailBellData = [];
+                emailBellData = data;
+                console.log(data);
+                var bell_count;
+                if (data.length == 1) {
+                    if (emailBellData[0].status === "unread") {
+                        bell_count = parseInt(emailBellData[0].unreadCount);
+                        console.log(emailBellData[0].unreadCount);
+                        document.querySelector(".emailBill").innerText = `${bell_count}`;
+                    } else {
+                        document.querySelector(".emailBill").innerText = "";
+                    }
+                } else if (data.length == 2) {
+                    if (emailBellData[0].status === "unread") {
+                        var bell_count1 = parseInt(emailBellData[0].unreadCount);
+                        console.log(bell_count1);
+                    } else {
+                        var bell_count1 = 0;
+                        document.querySelector(".emailBill").innerText = "";
+                    }
 
-            if (emailBellData[1].status === "unread") {
-                var bell_count2 = parseInt(emailBellData[1].unreadCount);
-                console.log(bell_count2);
-            } else {
-                var bell_count2 = 0;
-                document.querySelector(".emailBill").innerText = "";
-            }
+                    if (emailBellData[1].status === "unread") {
+                        var bell_count2 = parseInt(emailBellData[1].unreadCount);
+                        console.log(bell_count2);
+                    } else {
+                        var bell_count2 = 0;
+                        document.querySelector(".emailBill").innerText = "";
+                    }
 
-            bell_count = bell_count1 + bell_count2;
-            console.log(bell_count);
+                    bell_count = bell_count1 + bell_count2;
+                    console.log(bell_count);
 
-            if (bell_count !== "undefined" && bell_count > 0) {
-                document.querySelector(".emailBill").innerText = `${bell_count}`;
-            }
-        } else {
+                    if (bell_count !== "undefined" && bell_count > 0) {
+                        document.querySelector(".emailBill").innerText = `${bell_count}`;
+                    }
+                } else {
+                    document.querySelector(".emailBill").innerText = "";
+                }
+            });
+
+        document.querySelector(".bi-bell").addEventListener("click", function () {
             document.querySelector(".emailBill").innerText = "";
-        }
-    });
 
-document.querySelector(".bi-bell").addEventListener("click", function () {
-    document.querySelector(".emailBill").innerText = "";
-
-    console.log(userId);
-    fetch(
-        `/elitebaby/report/emailservlet?action=get_email_redis&userId=user${userId}`,
-        {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        }
-    )
-        .then((resp) => resp.json())
-        .then((data) => {
-            let emailBellData = [];
-            emailBellData = data;
-            console.log(data);
-            if (data.length == 0) {
-                document.querySelector(
-                    "#popupContent"
-                ).innerHTML = `<a href="/elitebaby/visit/ReportEmailFrontRSMail.html" style="display: block; margin-bottom: 10px;">"您的收件匣有0封未讀信件"</a>
+            console.log(userId);
+            fetch(
+                `/elitebaby/report/emailservlet?action=get_email_redis&userId=user${userId}`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            )
+                .then((resp) => resp.json())
+                .then((data) => {
+                    let emailBellData = [];
+                    emailBellData = data;
+                    console.log(data);
+                    if (data.length == 0) {
+                        document.querySelector(
+                            "#popupContent"
+                        ).innerHTML = `<a href="/elitebaby/visit/ReportEmailFrontRSMail.html" style="display: block; margin-bottom: 10px;">"您的收件匣有0封未讀信件"</a>
                                                               <a href="/elitebaby/visit/ReportEmailFrontGetAll.html">"您的寄件匣有0封回覆信件"</a>`;
-            } else if (data.length == 1) {
-                for (let xe = 0; xe < emailBellData.length; xe++) {
-                    if (emailBellData[xe].from === "admin") {
-                        var str1 = `<a href="/elitebaby/visit/ReportEmailFrontRSMail.html" style="display: block; margin-bottom: 10px;">"您的收件匣有${emailBellData[xe].unreadCount}封未讀信件"</a>`;
-                        var str2 = `<a href="/elitebaby/visit/ReportEmailFrontGetAll.html">"您的寄件匣有0封回覆信件"</a>`;
+                    } else if (data.length == 1) {
+                        for (let xe = 0; xe < emailBellData.length; xe++) {
+                            if (emailBellData[xe].from === "admin") {
+                                var str1 = `<a href="/elitebaby/visit/ReportEmailFrontRSMail.html" style="display: block; margin-bottom: 10px;">"您的收件匣有${emailBellData[xe].unreadCount}封未讀信件"</a>`;
+                                var str2 = `<a href="/elitebaby/visit/ReportEmailFrontGetAll.html">"您的寄件匣有0封回覆信件"</a>`;
+                            } else {
+                                var str1 = `<a href="/elitebaby/visit/ReportEmailFrontRSMail.html" style="display: block; margin-bottom: 10px;">"您的收件匣有0封未讀信件"</a>`;
+                                var str2 = `<a href="/elitebaby/visit/ReportEmailFrontGetAll.html">"您的寄件匣有${emailBellData[xe].unreadCount}封回覆信件"</a>`;
+                            }
+                        }
+                        var compelt = str1 + str2;
+                        document.querySelector("#popupContent").innerHTML = compelt;
                     } else {
-                        var str1 = `<a href="/elitebaby/visit/ReportEmailFrontRSMail.html" style="display: block; margin-bottom: 10px;">"您的收件匣有0封未讀信件"</a>`;
-                        var str2 = `<a href="/elitebaby/visit/ReportEmailFrontGetAll.html">"您的寄件匣有${emailBellData[xe].unreadCount}封回覆信件"</a>`;
+                        for (let xe = 0; xe < emailBellData.length; xe++) {
+                            if (emailBellData[xe].from === "admin") {
+                                var str1 = `<a href="/elitebaby/visit/ReportEmailFrontRSMail.html" style="display: block; margin-bottom: 10px;">"您的收件匣有${emailBellData[xe].unreadCount}封未讀信件"</a>`;
+                                console.log(
+                                    "您的收件匣有" + emailBellData[xe].unreadCount + "封未讀信件"
+                                );
+                            } else {
+                                var str2 = `<a href="/elitebaby/visit/ReportEmailFrontGetAll.html">"您的寄件匣有${emailBellData[xe].unreadCount}封回覆信件"</a>`;
+                                console.log(
+                                    "您的寄件匣有" + emailBellData[xe].unreadCount + "封回覆信件"
+                                );
+                            }
+                        }
+                        var compelt = str1 + str2;
+                        document.querySelector("#popupContent").innerHTML = compelt;
                     }
+                });
+
+
+            fetch(
+                `/elitebaby/report/emailservlet?action=get_email_chang_status&userId=user${userId}`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
                 }
-                var compelt = str1 + str2;
-                document.querySelector("#popupContent").innerHTML = compelt;
+            )
+                .then((resp) => resp.json())
+                .then((data) => { });
+
+
+
+
+        });
+
+
+        const bellbtn = document.querySelector(".bi-bell");
+        const popupWrapper = document.getElementById("popupWrapper");
+        document.querySelector(".bi-bell").onclick = function () {
+            // 顯示小小的視窗
+            if (popupWrapper.style.display === "block") {
+                popupWrapper.style.display = "none";
             } else {
-                for (let xe = 0; xe < emailBellData.length; xe++) {
-                    if (emailBellData[xe].from === "admin") {
-                        var str1 = `<a href="/elitebaby/visit/ReportEmailFrontRSMail.html" style="display: block; margin-bottom: 10px;">"您的收件匣有${emailBellData[xe].unreadCount}封未讀信件"</a>`;
-                        console.log(
-                            "您的收件匣有" + emailBellData[xe].unreadCount + "封未讀信件"
-                        );
-                    } else {
-                        var str2 = `<a href="/elitebaby/visit/ReportEmailFrontGetAll.html">"您的寄件匣有${emailBellData[xe].unreadCount}封回覆信件"</a>`;
-                        console.log(
-                            "您的寄件匣有" + emailBellData[xe].unreadCount + "封回覆信件"
-                        );
-                    }
-                }
-                var compelt = str1 + str2;
-                document.querySelector("#popupContent").innerHTML = compelt;
+                popupWrapper.style.display = "block";
+            }
+        };
+
+        document.addEventListener("click", function (event) {
+            if (event.target !== popupWrapper && bellbtn !== event.target) {
+                popupWrapper.style.display = "none";
             }
         });
-});
+
+        document.addEventListener("keydown", (event) => {
+            if (event.key === "Escape") {
+                popupWrapper.style.display = "none";
+            }
+        });
+
+
+    });
+// ================================================================================================
 
 // 確認登入狀態
 function checkLogin() {
@@ -254,40 +315,9 @@ function getAPI() {
     });
 }
 
-fetch(
-    `/elitebaby/report/emailservlet?action=get_email_chang_status&userId=user${userId}`,
-    {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }
-)
-    .then((resp) => resp.json())
-    .then((data) => { });
 
-const bellbtn = document.querySelector(".bi-bell");
-const popupWrapper = document.getElementById("popupWrapper");
-document.querySelector(".bi-bell").onclick = function () {
-    // 顯示小小的視窗
-    if (popupWrapper.style.display === "block") {
-        popupWrapper.style.display = "none";
-    } else {
-        popupWrapper.style.display = "block";
-    }
-};
 
-document.addEventListener("click", function (event) {
-    if (event.target !== popupWrapper && bellbtn !== event.target) {
-        popupWrapper.style.display = "none";
-    }
-});
 
-document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
-        popupWrapper.style.display = "none";
-    }
-});
 
 // --------- 會員首頁js --------------
 function callAPI() {
