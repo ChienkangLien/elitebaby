7266a0bee30d63289eb03e4c801fff38c8eba502
Howard Lien <chienkang1114@gmail.com>
2023-03-23 23:05:19 +0800

    style: 遮蔽無效標籤

--- src/main/webapp/js/background_navbar.js
+++ src/main/webapp/js/background_navbar.js
@@ -1,3 +1,7 @@
 $("button.btn_logout").on("click", function () {
   location.href = "/elitebaby/admin/member/emplogout";
 });
+
+$(document).ready(function () {
+  $("a[href='/elitebaby/admin/forum/backend.html']").css("display", "none");
+});
